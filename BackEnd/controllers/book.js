2c6e729889bf6446d31abc12708a22c196f91309
Sylvain Cheveny <scheveny@gmail.com>
2024-01-08 15:03:27 +0100

    feat sharp to resize images

--- BackEnd/controllers/book.js
+++ BackEnd/controllers/book.js
@@ -1,7 +1,7 @@
 const Book = require('../models/book');
 const fs = require('fs');
-const auth = require('../middleware/auth');
-const jwt = require('jsonwebtoken');
+const sharp = require('sharp');
+
 
 exports.getBooks = (req, res, next) => {
     Book.find()
@@ -19,12 +19,31 @@ exports.createBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
     delete bookObject._id;
     delete bookObject.userId;
-  
+    console.log(req.file);
     const book = new Book({
       ...bookObject,
-      imageUrl: `${req.protocol}://${req.get('host')}/${req.imagePath}`, 
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`, 
       userId: req.auth.userId,
     });
+
+    // Redimensionne l'image, la convertit en webp et supprime l'image originale
+    sharp(req.file.path)
+        .resize(700)
+        .webp({ quality: 60 })
+        .toFile(`images/${req.file.filename}.webp`, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Image resized and converted to webp successfully');
+                fs.unlink(req.file.path, err => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log('Original image deleted successfully');
+                    }
+                });
+            }
+        });
   
     book.save()
       .then(() => res.status(201).json({ message: 'Livre enregistré avec succès !' })) 
@@ -54,7 +73,7 @@ exports.modifyBook = (req, res, next) => {
     if (req.file) {
         bookObject = {
             ...JSON.parse(req.body.book),
-            imageUrl: `${req.protocol}://${req.get('host')}/${req.imagePath}`, 
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`, 
         };
     } else {
         // Si aucune image n'est fournie, prenez directement les informations du corps de la requête.
@@ -72,6 +91,25 @@ exports.modifyBook = (req, res, next) => {
             if (book.userId !== req.auth.userId) {
                 return res.status(403).json({ message: 'Requête non autorisée !' });
             }
+
+            // Redimensionne l'image, la convertit en webp et supprime l'image originale
+            sharp(req.file.path)
+            .resize(700)
+            .webp({ quality: 60 })
+            .toFile(`images/${req.file.filename}.webp`, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Image resized and converted to webp successfully');
+                    fs.unlink(req.file.path, err => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log('Original image deleted successfully');
+                        }
+                    });
+                }
+            });
 
             // Mise à jour du livre.
             Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
--- BackEnd/routes/book.js
+++ BackEnd/routes/book.js
@@ -11,5 +11,6 @@ router.get('/:id', bookCtrl.getOneBook);
 router.post('/', auth, multer, bookCtrl.createBook);
 router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
+router.post('/:id/rating', auth, bookCtrl.rateBook);
 
 module.exports = router;
